f78d0b851c2f2ce14e90787273aea284e1f3b3d0
Peter Gabrielsen <peterg@cs.au.dk>
2015-10-16 11:36:31 -0700

    fixed dk plots

--- res/dk/plot_random_fixed_size.sh
+++ res/dk/plot_random_fixed_size.sh
@@ -3,7 +3,7 @@ set terminal pngcairo enhanced font 'Verdana,12'
 set title "Decrease key on 2^{15} random"
 set output 'dk_random_fixed_size.png'
 set xlabel "N (# of decrease key operations)"
-set ylabel "running time (ns per decrease key)"
+set ylabel "running time (ms)"
 set style line 1 lc rgb '#0000FF' pt 5 ps 1 lt 1 lw 1
 set style line 2 lc rgb '#00FF00' pt 4 ps 1 lt 1 lw 1
 set style line 3 lc rgb '#FF0000' pt 8 ps 1 lt 1 lw 1
@@ -15,6 +15,6 @@ set logscale x 2
 #set logscale y 2
 set format x "2^{%L}"
 set key left top
-plot "ndk_random_bh.dat" using 5:($2/$5 * 1000) ls 1 with linespoints title 'binary heap' , \
-     "ndk_random_fq.dat" using 5:($2/$5 * 1000) ls 2 with linespoints title 'fibonacci heap'
+plot "ndk_random_bh.dat" using 5:($2) ls 1 with linespoints title 'binary heap' , \
+     "ndk_random_fq.dat" using 5:($2) ls 2 with linespoints title 'fibonacci heap'
 
